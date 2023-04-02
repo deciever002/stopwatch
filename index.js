7f36ee335096ac9e28e607b0d8702bf5d6a07ccf
manastiwari002 <manastiwari002@gmail.com>
2023-04-03 02:07:07 +0530

    Created the project and added HTML and css to it

---
+++ index.js
@@ -0,0 +1,9 @@
+//Grab the timer minutes and seconds
+let timer = document.getElementById("timer");
+
+//Grab all the buttons 
+let start = document.getElementById("start");
+let stop = document.getElementById("stop");
+let reset = document.getElementById("reset");
+
+//Add Event Listeners to the buttons
