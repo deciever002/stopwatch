1544c4355f9c4cac7fed27ddc665f5dddd34b119
manastiwari002 <manastiwari002@gmail.com>
2023-04-03 02:38:35 +0530

    added javascript file for controlling the behaviour

--- index.js
+++ index.js
@@ -1,9 +1,49 @@
+//Set the initial state of seconds and milliseconds in timer
+let [seconds,milliseconds] = [0,0];
+
 //Grab the timer minutes and seconds
 let timer = document.getElementById("timer");
 
 //Grab all the buttons 
-let start = document.getElementById("start");
-let stop = document.getElementById("stop");
-let reset = document.getElementById("reset");
+let startTimer = document.getElementById("start");
+let stopTimer = document.getElementById("stop");
+let resetTimer = document.getElementById("reset");
+let interval;
 
 //Add Event Listeners to the buttons
+//Start the stopwatch if start button is clicked
+startTimer.addEventListener("click",() => {
+    console.log("Started the Stopwatch");
+    if(interval !== null){
+        clearInterval(interval);
+    }
+    interval = setInterval(startWatch,10);
+});
+
+//Stop the stopwatch if stop is clicked
+stopTimer.addEventListener('click',()=>{
+    console.log("Stopwatch Stopped!");
+    clearInterval(interval);
+});
+
+//Reset the stopwatch back to default or initial values if reset button is clicked
+resetTimer.addEventListener('click',() => {
+    console.log("Stopwatch Resetted!")
+    clearInterval(interval);
+    [seconds,milliseconds] = [0,0];
+    timer.innerHTML = '00:00';
+});
+
+//Set Interval calls the start watch function which starts the stopwatch 
+//This function is called after every 10ms unless someone stops or resets the stopwatch
+function startWatch(){
+    milliseconds++;
+    if(milliseconds == 100){
+        seconds++;
+        milliseconds = 0;
+        seconds == 60 ? seconds = 0 : seconds; 
+    }
+    let s = seconds < 10 ? "0" + seconds : seconds;
+    let ms = milliseconds < 10 ? "0" + milliseconds : milliseconds;
+    timer.innerHTML = `${s}:${ms}`;
+}
